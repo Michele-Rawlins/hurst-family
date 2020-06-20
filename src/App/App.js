f9aa1f3fd86bb5640a4de8ba0e2572745cac3c90
Michele Rawlins <shellbop@att.net>
2020-06-20 11:13:09 -0500

    set up firebase

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>INSIDE APP COMPONENT</h2>
+    <button className="btn btn-info"><i className="fas fa-rocket"></i></button>
+  </div>
+  );
+}
+
+export default App;
