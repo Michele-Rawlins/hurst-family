6b87a8ec6f599628e6f6c94cd74e59fd1a18df30
Michele Rawlins <shellbop@att.net>
2020-06-25 21:44:23 -0500

    wip

--- src/components/pages/FamilyMembers/FamilyMembers.js
+++ src/components/pages/FamilyMembers/FamilyMembers.js
@@ -7,6 +7,7 @@ import FamilyMembersCard from '../../shared/FamilyMembersCard/FamilyMembersCard'
 class FamilyMembers extends React.Component {
   state={
     familyMembers: [],
+   
   }
   getFamilyMembers =() => {
     const uid = authData.getUid();
@@ -26,16 +27,18 @@ class FamilyMembers extends React.Component {
   
   }
 
+
   render() {
     const { familyMembers } =this.state;
-    const buildFamilyMembersCard = familyMembers.map((familyMembers) => (
+      const buildFamilyMembersCard = familyMembers.map((familyMembers) => (
+
       <FamilyMembersCard key={familyMembers.id} familyMembers={familyMembers} removeFamilyMember={this.removeFamilyMember}/>
     ));
     return (
       <div className="FamilyMembers">
         <h1>Family Members</h1>
         <div className="d-flex flex-wrap">
-          {buildFamilyMembersCard}
+        {buildFamilyMembersCard}
         </div>
       </div>
     );
