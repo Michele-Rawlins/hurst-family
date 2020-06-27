a8e7f0a0bba670eaa4b9710dddce251df63780cc
Michele Rawlins <shellbop@att.net>
2020-06-26 22:00:30 -0500

    added data and fixed typo

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -14,8 +14,8 @@ class Home extends React.Component {
         <h1 className="hurst-homepage">Welcome to the Hurst Family</h1>
         <p className="arabian-proverb">Every day of your life is a page of your history. -- Arabian Proverb</p>
         <img className="JamesandElsiePic" src="https://live.staticflickr.com/65535/50027351133_6890848f92_n.jpg" width="400" height="350" alt="jamesAndElsieCloseUp"></img>
-        <p className="hurst-beginnings align-left"> In a little farmhouse in Hinkle Haller, the next generation of the Hurst family began. They farmed the land from to survive.  They only went to town for salt, sugar and flour.  They ground their own meal.  They all rose from humble beginnings.  Each was a success in their own right.</p>
-        <p className="hurst-beginnings align-left">James Hurst worked in the mines to save enough money to purchase the farm and then built the home pictured below.</p>
+        <p className="hurst-beginnings align-left"> In a little farmhouse in Hinkle Haller, the next generation of the Hurst family began. They farmed the land from to survive.  They only went to town for salt, sugar and flour.  They ground their own meal.  They all rose from humble beginnings.  Each was a success in their own right.
+        <p className="hurst-beginnings align-left">James Hurst worked in the mines to save enough money to purchase the farm and then built the home pictured below.</p></p>
         <img className="hurst-home-dog align-right" src="https://live.staticflickr.com/65535/50027350968_6cfbcd7cce_n.jpg" width="320" height="318" alt="dogOnPorch"></img>
         <img className="hurst-home align-right" src="https://live.staticflickr.com/65535/50021056126_fc9fb6d89f_n.jpg" width="320" height="318" alt="hursthome"></img>
         <img className="hurst-farm align-right" src="https://live.staticflickr.com/65535/50021056226_221731e6c5_n.jpg" width="320" height="318" alt="familyfarm"></img>
